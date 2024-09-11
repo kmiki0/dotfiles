5aac6f188f422961237f60d02e6bb90154a49a8e
kmiki0 <kmiki@anvil.ne.jp>
2024-09-11 12:15:37 +0000

    hlchink.lua merge

--- .vim/lua/plugins/plugins_list.lua
+++ .vim/lua/plugins/plugins_list.lua
@@ -9,4 +9,33 @@ return {
       require('plugins.lualine')
     end,
   },
+  -- neo-tree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- not strictly required, but recommended
+    },
+    config = function()
+      require('plugins.neotree')
+    end,
+  },
+  -- hlchunk
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("plugins.hlchunk")
+    end
+  },
+  -- Comment
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    }
+  },
 }
